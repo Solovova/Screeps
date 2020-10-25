7a8fbfed682b83e871c190de752dae8da244fe41
solovova <vsolo0@gmail.com>
2020-10-25 11:36:06 +0200

    1.20

--- src/main/kotlin/accounts/srvMain2/Main2Constants.kt
+++ src/main/kotlin/accounts/srvMain2/Main2Constants.kt
@@ -10,9 +10,9 @@ fun AccountInitMain2.initHeadOut(const: Constants) {
                                                //M0     M1     M2     M3     M4     M5     M6
     const.initMainRoomConstantContainer(arrayOf("W5N3","W4N3","W6N3","W7N3","W7N4","W7N5","W3N5"))
 
-    const.getMainRoomConstant("W5N3").initSlaveRoomConstantContainer(arrayOf("W5N2","W5N4","W6N4"))          //M0
-    const.getMainRoomConstant("W4N3").initSlaveRoomConstantContainer(arrayOf("W4N2"))                 //M1
-    const.getMainRoomConstant("W6N3").initSlaveRoomConstantContainer(arrayOf())                       //M2
+    const.getMainRoomConstant("W5N3").initSlaveRoomConstantContainer(arrayOf("W5N2","W5N4"))          //M0
+    const.getMainRoomConstant("W4N3").initSlaveRoomConstantContainer(arrayOf("W4N2","W4N4"))          //M1
+    const.getMainRoomConstant("W6N3").initSlaveRoomConstantContainer(arrayOf("W6N4"))                 //M2
     const.getMainRoomConstant("W7N3").initSlaveRoomConstantContainer(arrayOf("W8N3"))                 //M3
     const.getMainRoomConstant("W7N4").initSlaveRoomConstantContainer(arrayOf("W8N4"))                 //M4
     const.getMainRoomConstant("W7N5").initSlaveRoomConstantContainer(arrayOf("W8N5","W7N6"))          //M5
@@ -30,13 +30,13 @@ fun AccountInitMain2.initBodyOut(const: Constants) {
     const.m(4).useCash = false
     const.m(5).useCash = false
     const.m(6).useCash = false
-    const.globalConstant.terminalPriorityRoom = ""
+    const.globalConstant.terminalPriorityRoom = "W3N5"
 
     const.s(0,2).model = SlaveRoomType.Colonize
 
     const.m(5).defenceLimitUpgrade = 1000000
 
-    const.globalConstant.defenceLimitUpgrade = 10000000
+    const.globalConstant.defenceLimitUpgrade = 17000000
     const.globalConstant.balanceQtyUpgraderDefault = 12
     const.globalConstant.balanceQtyBuilderDefault = 1
     const.globalConstant.nukerFill = false
@@ -49,7 +49,7 @@ fun AccountInitMain2.initBodyOut(const: Constants) {
     const.m(3).reactionActiveArr = arrayOf("ZK", "UL", "G", "GH", "OH", "GH2O", "XGH2O")
     const.m(4).reactionActiveArr = arrayOf("ZK", "UL", "G", "GH", "OH", "GH2O", "XGH2O")
     const.m(5).reactionActiveArr = arrayOf("LH", "LH2O", "XLH2O", "GH2O", "XGH2O")
-    const.m(6).reactionActiveArr = arrayOf("ZK", "UL", "G", "GH", "OH", "GH2O", "XGH2O")
+    const.m(6).reactionActiveArr = arrayOf("LH", "LH2O", "XLH2O", "GH2O", "XGH2O")
 
     const.globalConstant.creepUpgradablePartsRange[19] = mapOf<BodyPartConstant, List<Pair<Int, ResourceConstant>>>(
             WORK to
@@ -63,7 +63,7 @@ fun AccountInitMain2.initBodyOut(const: Constants) {
     const.globalConstant.creepUpgradablePartsRange[7] = mapOf<BodyPartConstant, List<Pair<Int, ResourceConstant>>>(
             WORK to
                     listOf(
-                            Pair(100000, "XGH2O".unsafeCast<ResourceConstant>()),
+                            Pair(10000, "XGH2O".unsafeCast<ResourceConstant>()),
                             Pair(300000, "GH2O".unsafeCast<ResourceConstant>()),
                             Pair(300000, "GH".unsafeCast<ResourceConstant>())
                     )
